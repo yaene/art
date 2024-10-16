1d62bd6e7cb8f86799b6697abaa1b7ac747f2cd2
Santiago Aboy Solanes <solanes@google.com>
2024-10-16 15:09:22 +0100

    Fix marking methods as always throws
    Bug: 373867314
    Fixes: 373867314
    Test: art/test/testrunner/testrunner.py --host --64 -b --optimizing
    Change-Id: I18b5e03021e6eec841050bd39c79480c0378dda2

--- compiler/optimizing/inliner.cc
+++ compiler/optimizing/inliner.cc
@@ -422,7 +422,13 @@ static bool AlwaysThrows(ArtMethod* method)
   if (!method->IsCompilable() || !IsMethodVerified(method)) {
     return false;
   }
+
   // Skip native methods, methods with try blocks, and methods that are too large.
+  // TODO(solanes): We could correctly mark methods with try/catch blocks as always throwing as long
+  // as we can get rid of the infinite loop cases. These cases (e.g. `void foo() {while (true) {}}`)
+  // are the only ones that can have no return instruction and still not be an "always throwing
+  // method". Unfortunately, we need to construct the graph to know there's an infinite loop and
+  // therefore not worth the trouble.
   CodeItemDataAccessor accessor(method->DexInstructionData());
   if (!accessor.HasCodeItem() ||
       accessor.TriesSize() != 0 ||
@@ -1993,11 +1999,13 @@ bool HInliner::CanInlineBody(const HGraph* callee_graph,
   }
 
   bool has_one_return = false;
+  bool has_try_catch = false;
   for (HBasicBlock* predecessor : exit_block->GetPredecessors()) {
     const HInstruction* last_instruction = predecessor->GetLastInstruction();
     // On inlinees, we can have Return/ReturnVoid/Throw -> TryBoundary -> Exit. To check for the
     // actual last instruction, we have to skip the TryBoundary instruction.
     if (last_instruction->IsTryBoundary()) {
+      has_try_catch = true;
       predecessor = predecessor->GetSinglePredecessor();
       last_instruction = predecessor->GetLastInstruction();
 
@@ -2039,7 +2047,9 @@ bool HInliner::CanInlineBody(const HGraph* callee_graph,
   }
 
   if (!has_one_return) {
-    if (!is_speculative) {
+    // If a method has a try catch, all throws are potentially caught. We are conservative and
+    // don't assume a method always throws unless we can guarantee that.
+    if (!is_speculative && !has_try_catch) {
       // If we know that the method always throws with the particular parameters, set it as such.
       // This is better than using the dex instructions as we have more information about this
       // particular call. We don't mark speculative inlines (e.g. the ones from the inline cache) as
@@ -2048,6 +2058,13 @@ bool HInliner::CanInlineBody(const HGraph* callee_graph,
       graph_->SetHasAlwaysThrowingInvokes(/* value= */ true);
     }
 
+    // Methods that contain infinite loops with try catches fall into this line too as we construct
+    // an Exit block for them. This will mean that the stat `kNotInlinedAlwaysThrows` might not be
+    // 100% correct but:
+    // 1) This is a very small fraction of methods, and
+    // 2) It is not easy to disambiguate between those.
+    // Since we want to avoid inlining methods with infinite loops anyway, we return false for these
+    // cases too.
     LOG_FAIL(stats_, MethodCompilationStat::kNotInlinedAlwaysThrows)
         << "Method " << resolved_method->PrettyMethod()
         << " could not be inlined because it always throws";
